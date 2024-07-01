cfdb268559cbe9f0777f0c1bcea75327397ffc87
NgHuy31 <143270164+NgHuy31@users.noreply.github.com>
2024-07-02 00:43:47 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignInScreen from './screens/SignInScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="SignIn">
+                <Stack.Screen name="SignIn" component={SignInScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
